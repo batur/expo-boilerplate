bcadf57114257a4b4b05b17880c9460a711b5379
Batur <halidbaturakkurt@gmail.com>
2025-01-16 19:15:28 +0300

    feat: tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,16 @@ import "../global.css";
 
 import { Stack } from "expo-router";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
